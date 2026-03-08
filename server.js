017fa585d5174c0ca403226ab029996fc31daf0d
ademabatouy08-creator <ademabatouy08@gmail.com>
2026-03-08 15:12:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,45 +6,46 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname));
 
 const users = {}; 
-const ADMIN_NAME = "toucheur2pp";
+const groups = {}; 
+const MY_NAME = "TonPseudo"; // ⚠️ METS TON PSEUDO ICI
 
 io.on('connection', (socket) => {
     const ip = (socket.handshake.headers['x-forwarded-for'] || socket.conn.remoteAddress || "0.0.0.0").split(',')[0].trim();
 
     socket.on('init_user', (data) => {
-        users[socket.id] = { ...data, ip, sid: socket.id };
-        
-        // On envoie la liste. Si c'est l'admin, il reçoit les IPs, sinon on les masque.
-        updateAllUsers();
+        users[socket.id] = { ...data, ip, sid: socket.id, friends: [], isAdmin: (data.name === MY_NAME) };
+        syncUsers();
     });
 
-    function updateAllUsers() {
+    function syncUsers() {
         Object.keys(users).forEach(sid => {
-            const isTargetAdmin = users[sid].name === ADMIN_NAME;
-            const userList = Object.values(users).map(u => ({
-                name: u.name,
-                pp: u.pp,
-                sid: u.sid,
-                theme: u.theme,
-                // L'IP n'est envoyée que si le destinataire est l'Admin
-                ip: isTargetAdmin ? u.ip : "SÉCURISÉ" 
+            const current = users[sid];
+            const list = Object.values(users).map(u => ({
+                name: u.name, pp: u.pp, sid: u.sid, 
+                ip: current.isAdmin ? u.ip : "HIDDEN" // SEUL TOI VOIS LES IP
             }));
-            io.to(sid).emit('update_users', userList);
+            io.to(sid).emit('update_users', list);
         });
     }
 
-    // --- SIGNALING WEBRTC ---
-    socket.on('call_user', (data) => {
-        io.to(data.to).emit('incoming_call', { signal: data.signal, from: socket.id, name: users[socket.id].name, pp: users[socket.id].pp });
+    // --- APPEL DISCORD-STYLE ---
+    socket.on('call_start', (d) => {
+        io.to(d.to).emit('incoming_call', { signal: d.signal, from: socket.id, name: users[socket.id].name, pp: users[socket.id].pp });
     });
-    socket.on('accept_call', (data) => { io.to(data.to).emit('call_accepted', data.signal); });
-    socket.on('ice_candidate', (data) => { io.to(data.to).emit('ice_candidate', data.candidate); });
 
-    socket.on('chat_msg', (data) => io.emit('chat_msg', data));
-    
-    socket.on('disconnect', () => { delete users[socket.id]; updateAllUsers(); });
+    socket.on('call_answer', (d) => {
+        io.to(d.to).emit('call_accepted', d.signal);
+    });
+
+    // --- GROUPES ---
+    socket.on('create_group', (name) => {
+        groups[name] = { members: [socket.id] };
+        io.emit('new_group', name);
+    });
+
+    socket.on('chat_msg', (d) => io.emit('chat_msg', d));
+
+    socket.on('disconnect', () => { delete users[socket.id]; syncUsers(); });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log("NEON_PREMIUM_V25_READY"));
-
+http.listen(3000, () => console.log("APEX_OMEGA_READY"));
