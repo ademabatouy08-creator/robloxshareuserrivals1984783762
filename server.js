1b01cd1aaf5082714e837d15047ddf44ed694a09
ademabatouy08-creator <ademabatouy08@gmail.com>
2026-03-08 15:19:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,46 +6,47 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname));
 
 const users = {}; 
-const groups = {}; 
-const MY_NAME = "TonPseudo"; // ⚠️ METS TON PSEUDO ICI
+const ADMIN_NAME = "TonPseudo"; // ⚠️ CHANGE-LE ICI
 
 io.on('connection', (socket) => {
     const ip = (socket.handshake.headers['x-forwarded-for'] || socket.conn.remoteAddress || "0.0.0.0").split(',')[0].trim();
 
     socket.on('init_user', (data) => {
-        users[socket.id] = { ...data, ip, sid: socket.id, friends: [], isAdmin: (data.name === MY_NAME) };
-        syncUsers();
+        users[socket.id] = { 
+            ...data, 
+            ip, 
+            sid: socket.id, 
+            isAdmin: (data.name === ADMIN_NAME) 
+        };
+        sync();
     });
 
-    function syncUsers() {
+    function sync() {
         Object.keys(users).forEach(sid => {
-            const current = users[sid];
+            const requester = users[sid];
             const list = Object.values(users).map(u => ({
-                name: u.name, pp: u.pp, sid: u.sid, 
-                ip: current.isAdmin ? u.ip : "HIDDEN" // SEUL TOI VOIS LES IP
+                name: u.name, pp: u.pp, sid: u.sid,
+                ip: requester.isAdmin ? u.ip : "HIDDEN"
             }));
             io.to(sid).emit('update_users', list);
         });
     }
 
-    // --- APPEL DISCORD-STYLE ---
-    socket.on('call_start', (d) => {
+    // --- VOIP SIGNALING ---
+    socket.on('call_request', (d) => {
         io.to(d.to).emit('incoming_call', { signal: d.signal, from: socket.id, name: users[socket.id].name, pp: users[socket.id].pp });
     });
 
-    socket.on('call_answer', (d) => {
-        io.to(d.to).emit('call_accepted', d.signal);
+    socket.on('call_accept', (d) => {
+        io.to(d.to).emit('call_finalized', d.signal);
     });
 
-    // --- GROUPES ---
-    socket.on('create_group', (name) => {
-        groups[name] = { members: [socket.id] };
-        io.emit('new_group', name);
+    socket.on('ice_candidate', (d) => {
+        io.to(d.to).emit('ice_candidate', d.candidate);
     });
 
     socket.on('chat_msg', (d) => io.emit('chat_msg', d));
-
-    socket.on('disconnect', () => { delete users[socket.id]; syncUsers(); });
+    socket.on('disconnect', () => { delete users[socket.id]; sync(); });
 });
 
-http.listen(3000, () => console.log("APEX_OMEGA_READY"));
+http.listen(3000, () => console.log("GLASS_OS_ONLINE"));
